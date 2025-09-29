4f9e05c3e6a8dc8e616fe096e861289c4c88daaa
Spolo <sergioepolo@gmail.com>
2025-09-28 22:37:30 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Admin } from './pages/admin/admin';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
+import { Products } from './pages/products/products';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home, title:'Welcome' },
+    { path: 'admin', component: Admin, title:'Admin Dashboard' },
+    { path: 'login', component: Login, title:'Login' },
+    { path: 'not_found', component: NotFound, title:'404 - Page not found' },
+    { path: 'products', component: Products, title:'Products' },
+    { path: 'register', component: Register, title:'Register user' },
+];
